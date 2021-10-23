a530c3f4ba648c229b22a78f3058791650e9d40f
keibi-waddle <kibo@waddlelab.com>
2021-10-23 23:54:43 +0900

    CellInfoView UI

--- Moyang/Cell/View/CellInfoView.swift
+++ Moyang/Cell/View/CellInfoView.swift
@@ -11,10 +11,12 @@ import SwiftUI
 struct CellInfoView: View {
     @ObservedObject var viewModel: CellInfoVM
     
-    private let questionList = ["내가 공경할 수 있는 사람은 어떤 유형의 사람인가요?",
+    private let questionList = ["내가 공경할 수 있는 사람은 어떤 유형의 사람인가요????????",
                                 "내가 공경하지 않은 사람은 어떤 유형의 사람인가요?",
                                 "성경에서 공경하라는 무슨 뜻인가요?"]
+    private let cellMember = ["기보1", "기보2", "기보3", "기보4", "기보5", "기보6", "기보7"]
     @State private var myThought = "asdasdasdasdsadsad\nasdasdas"
+    @State private var myPray = "asdasdas"
     @State private var showingMemo = true
     
     var body: some View {
@@ -39,15 +41,15 @@ struct CellInfoView: View {
                 })
                 .foregroundColor(Color.black)
             }
-            Divider()
             if showingMemo {
                 ScrollView(.vertical, showsIndicators: false) {
                     ForEach(questionList, id: \.self) { question in
-                        HStack(spacing: 10) {
+                        HStack {
                             Text(question)
                                 .frame(alignment: .topLeading)
                                 .font(.system(size: 16, weight: .regular, design: .default))
-                                .background(Color(Asset.bgColorGray.color))
+                                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+                                .background(Color.white)
                                 .cornerRadius(10)
                             Spacer()
                         }
@@ -55,9 +57,11 @@ struct CellInfoView: View {
                         TextEditor(text: $myThought)
                             .padding(.bottom, 20)
                             .frame(height: 200, alignment: .topLeading)
+                        Divider()
+                            .padding()
                     }
                 }
-                .frame(height: 500)
+                .frame(height: 400)
                 .transition(.asymmetric(insertion: .opacity, removal: .opacity))
                 .background(Color.white)
             }
@@ -65,14 +69,38 @@ struct CellInfoView: View {
                 Spacer()
                 NavigationLink(destination: CellSubjectHistoryView()) {
                     Text("모임 주제 모두 보기")
+                    Image(systemName: "book.fill")
                 }
             }
             Divider()
-            
+            HStack {
+                Text("셀 기도제목")
+                Spacer()
+            }
+            .padding(.top, 14)
+            ScrollView(.vertical, showsIndicators: false) {
+                ForEach(cellMember, id: \.self) { member in
+                    HStack {
+                        Text(member)
+                            .frame(alignment: .topLeading)
+                            .font(.system(size: 16, weight: .regular, design: .default))
+                            .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        Spacer()
+                    }
+                    TextEditor(text: $myPray)
+                        .padding(.bottom, 20)
+                        .frame(height: 200, alignment: .topLeading)
+                    Divider()
+                        .padding()
+                }
+            }
             Spacer()
         }
+        .foregroundColor(Color.black)
         .navigationBarTitleDisplayMode(.inline)
-        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        .padding(EdgeInsets(top: 14, leading: 20, bottom: 0, trailing: 20))
         .background(Color(Asset.bgColorGray.color))
     }
 }
