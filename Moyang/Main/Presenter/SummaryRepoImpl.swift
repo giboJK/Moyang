fd7f822b5fc3dc90b4547a7e3961b1de290176ec
keibi-waddle <kibo@waddlelab.com>
2021-12-17 23:11:35 +0900

    Firestore service

--- Moyang/Main/Presenter/SummaryRepoImpl.swift
+++ Moyang/Main/Presenter/SummaryRepoImpl.swift
@@ -19,26 +19,7 @@ class SummaryRepoImpl: SummaryRepo {
     
     func fetchSummary() -> AnyPublisher<Summary, MoyangError> {
 //        return Empty(completeImmediately: false).eraseToAnyPublisher()
-        return Future<Summary, MoyangError> { [weak self] promise in
-            guard let self = self else { return }
-            self.store.collection(self.collectionName)
-                .addSnapshotListener { querySnapshot, error in
-                    if let error = error {
-                        promise(.failure(MoyangError.other(error)))
-                    }
-                    if let query = querySnapshot {
-                        let summaryList = query.documents.compactMap { document in
-                            try? document.data(as: Summary.self)
-                        }
-                        if let summary = summaryList.first {
-                            promise(.success(summary))
-                        } else {
-                            promise(.failure(MoyangError.emptyData))
-                        }
-                    } else {
-                        promise(.failure(MoyangError.decodingFailed))
-                    }
-                }
-        }.eraseToAnyPublisher()
+        return FireStoreService.shared.fetchObject(collection: collectionName,
+                                                   type: Summary.self)
     }
 }
--- Moyang/Network/FireStoreService.swift
+++ Moyang/Network/FireStoreService.swift
@@ -10,12 +10,31 @@ import FirebaseFirestoreSwift
 import Combine
 
 class FireStoreService {
+    static let shared = FireStoreService()
     private let store = Firestore.firestore()
     
-    
-//    CollectionReference
-//    DocumentReference
-    
-    
-    
+    func fetchObject<T: Codable>(collection: String,
+                                 type: T.Type) -> AnyPublisher<T, MoyangError> {
+        return Future<T, MoyangError> { [weak self] promise in
+            guard let self = self else { return }
+            self.store.collection(collection)
+                .addSnapshotListener { querySnapshot, error in
+                    if let error = error {
+                        promise(.failure(MoyangError.other(error)))
+                    }
+                    if let query = querySnapshot {
+                        let summaryList = query.documents.compactMap { document in
+                            try? document.data(as: T.self)
+                        }
+                        if let summary = summaryList.first {
+                            promise(.success(summary))
+                        } else {
+                            promise(.failure(MoyangError.emptyData))
+                        }
+                    } else {
+                        promise(.failure(MoyangError.decodingFailed))
+                    }
+                }
+        }.eraseToAnyPublisher()
+    }
 }
