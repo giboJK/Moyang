78bdf177cc1e74d19d807e5a976f81c7252b021e
giboJK <teoeirm@gmail.com>
2022-01-14 15:10:08 +0900

    tabview(In progress)

---
+++ Moyang/Daily/View/DailyView.swift
@@ -0,0 +1,38 @@
+//
+//  DailyView.swift
+//  Moyang
+//
+//  Created by kibo on 2022/01/14.
+//
+
+import SwiftUI
+
+struct DailyView: View {
+    var body: some View {
+        ZStack {
+            Color(UIColor.bgColor).ignoresSafeArea()
+            VStack(spacing: 0) {
+                HStack {
+                    Text("Daily Bread")
+                        .font(.system(size: 24, weight: .black, design: .default))
+                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 0))
+                    Spacer()
+                }
+                DailySummaryView(viewModel: DailySummaryVM())
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 15, trailing: 0))
+                Divider()
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                MainCategoryList()
+                    .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
+            }
+            .padding(.top, 30)
+            .background(Color(UIColor.bgColor))
+        }
+    }
+}
+
+struct DailyView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyView()
+    }
+}
--- Moyang/Main/View/MainView.swift
+++ Moyang/Main/View/MainView.swift
@@ -9,24 +9,18 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        ZStack {
-            Color(UIColor.bgColor).ignoresSafeArea()
-            VStack(spacing: 0) {
-                HStack {
-                    Text("Daily Bread")
-                        .font(.system(size: 24, weight: .black, design: .default))
-                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 0))
-                    Spacer()
+        TabView {
+            DailyView()
+                .tabItem {
+                    Image(systemName: "note.text")
+                    Text("Daily")
                 }
-                DailySummaryView(viewModel: DailySummaryVM())
-                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 15, trailing: 0))
-                Divider()
-                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-                MainCategoryList()
-                    .padding(EdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0))
-            }
-            .padding(.top, 30)
-            .background(Color(UIColor.bgColor))
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                    Text("Profile")
+                }
         }
         .navigationBarHidden(true)
         .navigationBarTitleDisplayMode(.inline)
