76f3ea3fd89bc3d19de708174060aa46b14ccd42
giboJK <teoeirm@gmail.com>
2022-05-13 00:16:56 +0900

    Fix tab view

--- Moyang/Main/View/MainView.swift
+++ Moyang/Main/View/MainView.swift
@@ -10,30 +10,30 @@ import SwiftUI
 struct MainView: View {
     @Binding var rootIsActive: Bool
     
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
     var body: some View {
-        TabView {
-            NavigationView {
+        NavigationView {
+            TabView {
                 CommunityMainView()
                     .navigationBarHidden(true)
                     .navigationBarTitleDisplayMode(.inline)
-            }
-            .tabItem {
-                Image(uiImage: Asset.Images.Tabbar.cross.image)
-                Text("공동체")
-            }
-            
-            NavigationView {
+                    .tabItem {
+                        Image(uiImage: Asset.Images.Tabbar.cross.image)
+                        Text("공동체")
+                    }
+                
                 ProfileView(vm: ProfileVM(loginService: FSLoginService(service: FSServiceImpl())),
                             rootIsActive: $rootIsActive)
-                    .navigationBarHidden(true)
-                    .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
+                .navigationBarTitleDisplayMode(.inline)
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                    Text("Profile")
+                }
             }
-            .tabItem {
-                Image(systemName: "person.crop.circle.fill")
-                Text("Profile")
-            }
+            .accentColor(.ydGreen1)
         }
-        .accentColor(.ydGreen1)
     }
 }
 
