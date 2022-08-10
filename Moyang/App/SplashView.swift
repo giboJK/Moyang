9e06612be1140b47cd0a56733d374844fbf86e6f
giboJK <teoeirm@gmail.com>
2022-08-11 08:03:53 +0900

    Random string

--- Moyang/App/SplashView.swift
+++ Moyang/App/SplashView.swift
@@ -11,6 +11,11 @@ struct SplashView: View {
     @State var isCompleted = false
     @State var move = false
     @State private var timeRemaining = 1.4
+    
+    let randomInt = Int.random(in: 0..<4)
+    let middle = ["저는", "오늘도", "영원히", "제 삶을"]
+    let last = ["예배자입니다", "감사합니다", "사랑합니다", "드립니다"]
+    
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack(spacing: 0) {
@@ -31,7 +36,7 @@ struct SplashView: View {
             HStack(spacing: 0) {
                 Spacer()
                     .frame(width: 120)
-                Text("제 삶이")
+                Text(middle[randomInt])
                     .foregroundColor(.sheep1)
                     .font(.system(size: 20))
                     .fontWeight(.regular)
@@ -44,7 +49,7 @@ struct SplashView: View {
             HStack(spacing: 0) {
                 Spacer()
                     .frame(width: 120)
-                Text("예배입니다")
+                Text(last[randomInt])
                     .foregroundColor(.sheep1)
                     .font(.system(size: 26))
                     .fontWeight(.regular)
