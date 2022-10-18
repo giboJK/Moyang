dbfc3461ebf5a6b38e3c85417fca68c44a39bcb2
giboJK <teoeirm@gmail.com>
2022-10-18 23:48:03 +0900

    sleep!

--- Moyang/MyPray/MyPrayMain/View/MyPrayMainVC.swift
+++ Moyang/MyPray/MyPrayMain/View/MyPrayMainVC.swift
@@ -16,8 +16,39 @@ class MyPrayMainVC: UIViewController, VCType {
     var disposeBag: DisposeBag = DisposeBag()
     var vm: VM?
     var coordinator: MyPrayMainVCDelegate?
-
+    
+    let headerHeight: CGFloat = 184
+//    let headerHeight: CGFloat = 44
+    let minHeaderHeight: CGFloat = 44
     // MARK: - UI
+    let headerView = GroupPrayHeader()
+    let searchBar = MoyangSearchBar().then {
+        $0.isHidden = true
+    }
+    let prayTableView = UITableView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.register(GroupPrayTVCell.self, forCellReuseIdentifier: "cell")
+        $0.backgroundColor = .nightSky1
+        $0.separatorStyle = .none
+        $0.estimatedRowHeight = 220
+        $0.showsVerticalScrollIndicator = false
+        $0.bounces = true
+        $0.isScrollEnabled = true
+    }
+    let autoCompleteTableView = UITableView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.register(AutoCompleteTVCell.self, forCellReuseIdentifier: "cell")
+        $0.backgroundColor = .nightSky1
+        $0.separatorStyle = .none
+        $0.estimatedRowHeight = 48
+        $0.showsVerticalScrollIndicator = false
+        $0.bounces = true
+        $0.isScrollEnabled = true
+        $0.isHidden = true
+    }
+    let praySearchView = GroupPraySearchView().then {
+        $0.isHidden = true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +63,51 @@ class MyPrayMainVC: UIViewController, VCType {
         .darkContent
     }
     func setupUI() {
-        view.backgroundColor = .red
+        view.backgroundColor = .nightSky1
+        setupPrayTableView()
+        setupSearchBar()
+        setupPraySearchView()
+        setupAutoCompleteTableView()
+    }
+    private func setupSearchBar() {
+        view.addSubview(searchBar)
+        searchBar.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(8)
+            $0.height.equalTo(36)
+            $0.left.right.equalToSuperview().inset(20)
+        }
+    }
+    private func setupAutoCompleteTableView() {
+        view.addSubview(autoCompleteTableView)
+        autoCompleteTableView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(8)
+            $0.left.right.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+    private func setupPraySearchView() {
+        view.addSubview(praySearchView)
+        praySearchView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(12)
+            $0.left.right.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+//        praySearchView.vm = vm
+        praySearchView.bind()
+    }
+    private func setupPrayTableView() {
+        view.addSubview(prayTableView)
+        prayTableView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.left.right.equalToSuperview()
+        }
+        prayTableView.stickyHeader.view = headerView
+        prayTableView.stickyHeader.height = headerHeight
+        prayTableView.stickyHeader.minimumHeight = minHeaderHeight
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 60)).then {
+            $0.backgroundColor = .clear
+        }
+        prayTableView.tableFooterView = footer
     }
 
     // MARK: - Binding
