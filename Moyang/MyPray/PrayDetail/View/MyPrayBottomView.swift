2e429ca72b810cea7e66d631e63382b7fe18eb23
giboJK <teoeirm@gmail.com>
2022-11-15 14:23:32 +0900

    UI...

--- Moyang/MyPray/PrayDetail/View/MyPrayBottomView.swift
+++ Moyang/MyPray/PrayDetail/View/MyPrayBottomView.swift
@@ -31,10 +31,25 @@ class MyPrayBottomView: UIView {
         super.init(frame: .zero)
         backgroundColor = .sheep2
         setupUI()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        hidePrayButton()
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if textView.textView.text.isEmpty {
+            showPrayButton()
+        }
     }
     
     private func setupUI() {
@@ -83,6 +98,28 @@ class MyPrayBottomView: UIView {
             $0.left.equalTo(typeLabel.snp.right).offset(4)
             $0.centerY.right.equalToSuperview()
             $0.size.equalTo(8)
+        }
+    }
+    
+    private func hidePrayButton() {
+        textView.snp.updateConstraints {
+            $0.right.equalToSuperview().inset(12)
+        }
+        prayButton.isHidden = true
+        UIView.animate(withDuration: 0.5) {
+            self.updateConstraints()
+            self.layoutIfNeeded()
+        }
+    }
+    
+    private func showPrayButton() {
+        textView.snp.updateConstraints {
+            $0.right.equalToSuperview().inset(120)
+        }
+        prayButton.isHidden = false
+        UIView.animate(withDuration: 0.5) {
+            self.updateConstraints()
+            self.layoutIfNeeded()
         }
     }
 }
